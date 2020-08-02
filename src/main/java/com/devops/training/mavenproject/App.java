905e514606883eec266731498e57d15ee2c08b77
Ubuntu <munawircm2mindt@munawircm2mindt.jcm4dlu4cczencb1n52dlttdfd.gx.internal.cloudapp.net>
2020-08-02 14:46:36 +0000

    First maven project

---
+++ src/main/java/com/devops/training/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.devops.training.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
