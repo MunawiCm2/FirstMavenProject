1ae7c808a680fd40615d472af750b9b927a1ac99
Ubuntu <munawircm2mindt@munawircm2mindt.jcm4dlu4cczencb1n52dlttdfd.gx.internal.cloudapp.net>
2020-08-02 15:39:57 +0000

    Email trigger

--- src/main/java/com/devops/training/mavenproject/App.java
+++ src/main/java/com/devops/training/mavenproject/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	// default 
+    	// default  
         System.out.println( "Hello World! made this change " );
     }
 }
